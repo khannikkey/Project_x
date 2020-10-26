6829331b9647134cbc377b7ccdfbaaaae977f094
khannikkey <nadeem.1053@gmail.com>
2020-10-26 10:12:46 +0530

    FinaleCommit

--- Ower/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OwerConfig(AppConfig):
-    name = 'Ower'
--- Ower/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from Ower import views
-
-urlpatterns = [
-    path('', views.index ),
-]
